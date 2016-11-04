38d09732ef3f80b02fe43e36c62a95555650e1ec
bjoynes <blakejoynes@gmail.com>
2016-11-04 15:55:39 -0400

    Added controller for user login and the main content
    Also added the views for each

--- src/main/Controller/LoginController.java
+++ src/main/Controller/LoginController.java
@@ -1,7 +1,24 @@
 package main.Controller;
-
+import javafx.event.*;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
 /**
  * Created by bjoynes on 11/4/2016.
  */
 public class LoginController {
+    @FXML private TextField userField;
+    @FXML private PasswordField passwordField;
+    @FXML private Button loginBtn;
+
+
+    @FXML private void login() {
+        String userName = userField.getText();
+        String password = passwordField.getText();
+
+      System.out.println("Success!");
+
+    }
+
+
+
 }
---
+++ src/main/Controller/MainController.java
@@ -0,0 +1,7 @@
+package main.Controller;
+
+/**
+ * Created by bjoynes on 11/4/2016.
+ */
+public class MainController {
+}
